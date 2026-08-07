1028aa22a3435172282de21a27eec6999e4adcef
laurentcartel-boop <laurentcartel@gmail.com>
2026-08-07 15:04:50 +0200

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -5,20 +5,33 @@ const Round = require('./Round');
 const RoundPlayer = require('./RoundPlayer');
 const HoleScore = require('./HoleScore');
 const IndexHistory = require('./IndexHistory');
+const Competition = require('./Competition');
 
-// Associations
+// Users / Rounds
 User.hasMany(Round, { foreignKey: 'created_by', as: 'createdRounds' });
 User.hasMany(Round, { foreignKey: 'closed_by', as: 'closedRounds' });
 User.hasMany(RoundPlayer, { foreignKey: 'user_id', as: 'roundParticipations' });
 User.hasMany(IndexHistory, { foreignKey: 'user_id', as: 'indexHistory' });
+User.hasMany(Competition, { foreignKey: 'created_by', as: 'createdCompetitions' });
 
+// Courses
 Course.hasMany(Round, { foreignKey: 'course_id', as: 'rounds' });
+Course.hasMany(Competition, { foreignKey: 'course_id', as: 'competitions' });
 Round.belongsTo(Course, { foreignKey: 'course_id', as: 'course' });
 
+// Rounds
 Round.belongsTo(User, { foreignKey: 'created_by', as: 'creator' });
 Round.belongsTo(User, { foreignKey: 'closed_by', as: 'closer' });
+Round.belongsTo(Competition, { foreignKey: 'competition_id', as: 'competition' });
 Round.hasMany(RoundPlayer, { foreignKey: 'round_id', as: 'players' });
 
+// Competitions
+Competition.belongsTo(Course, { foreignKey: 'course_id', as: 'course' });
+Competition.belongsTo(User, { foreignKey: 'created_by', as: 'creator' });
+Competition.belongsTo(User, { foreignKey: 'closed_by', as: 'closer' });
+Competition.hasMany(Round, { foreignKey: 'competition_id', as: 'squads' });
+
+// Round players / scores
 RoundPlayer.belongsTo(Round, { foreignKey: 'round_id', as: 'round' });
 RoundPlayer.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
 RoundPlayer.hasMany(HoleScore, { foreignKey: 'round_player_id', as: 'holeScores' });
@@ -37,4 +50,5 @@ module.exports = {
   RoundPlayer,
   HoleScore,
   IndexHistory,
+  Competition,
 };
