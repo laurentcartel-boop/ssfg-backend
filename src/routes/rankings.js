162f4b5e5d0ef9b43986cd8ff8b0a2eda8eb2abe
laurentcartel-boop <laurentcartel@gmail.com>
2026-08-07 18:21:14 +0200

    Update rankings.js

--- src/routes/rankings.js
+++ src/routes/rankings.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const rankingController = require('../controllers/rankingController');
 const { authenticate } = require('../middleware/auth');
 
-// Classements accessibles à tous les utilisateurs connectés
 router.use(authenticate);
 
 router.get('/', rankingController.getIndexRanking);
 router.get('/top', rankingController.getTop20);
+router.get('/season-years', rankingController.getSeasonYears);
+router.get('/season/:year', rankingController.getSeasonRanking);
 router.get('/categories/:category', rankingController.getCategoryRanking);
 
 module.exports = router;
