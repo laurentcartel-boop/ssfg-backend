049da3a6f899fb8bd29f5a35eb06ede27168cfc9
laurentcartel-boop <laurentcartel@gmail.com>
2026-09-05 14:16:00 +0200

    Export PushSubscription model

--- src/models/index.js
+++ src/models/index.js
@@ -26,32 +26,26 @@ try {
   MarcassinsTeam = require('./MarcassinsTeam');
   MarcassinsRegistration = require('./MarcassinsRegistration');
 } catch (e) {
-  console.warn('⚠️ Modèles Marcassins absents:', e.message);
+  console.warn('Modeles Marcassins absents:', e.message);
 }
 const AccountingEntry = require('./AccountingEntry');
 const Invoice = require('./Invoice');
 const RoundComment = require('./RoundComment');
 const RoundExploit = require('./RoundExploit');
+const PushSubscription = require('./PushSubscription');
 
-// Users / Rounds
 User.hasMany(Round, { foreignKey: 'created_by', as: 'createdRounds' });
 User.hasMany(Round, { foreignKey: 'closed_by', as: 'closedRounds' });
 User.hasMany(RoundPlayer, { foreignKey: 'user_id', as: 'roundParticipations' });
 User.hasMany(IndexHistory, { foreignKey: 'user_id', as: 'indexHistory' });
 User.hasMany(Competition, { foreignKey: 'created_by', as: 'createdCompetitions' });
-
-// Courses
 Course.hasMany(Round, { foreignKey: 'course_id', as: 'rounds' });
 Course.hasMany(Competition, { foreignKey: 'course_id', as: 'competitions' });
 Round.belongsTo(Course, { foreignKey: 'course_id', as: 'course' });
-
-// Rounds
 Round.belongsTo(User, { foreignKey: 'created_by', as: 'creator' });
 Round.belongsTo(User, { foreignKey: 'closed_by', as: 'closer' });
 Round.belongsTo(Competition, { foreignKey: 'competition_id', as: 'competition' });
 Round.hasMany(RoundPlayer, { foreignKey: 'round_id', as: 'players' });
-
-// Competitions
 Competition.belongsTo(Course, { foreignKey: 'course_id', as: 'course' });
 Competition.belongsTo(User, { foreignKey: 'created_by', as: 'creator' });
 Competition.belongsTo(User, { foreignKey: 'closed_by', as: 'closer' });
@@ -63,34 +57,24 @@ Competition.hasMany(CompetitionRegistration, { foreignKey: 'competition_id', as:
 CompetitionRegistration.belongsTo(Competition, { foreignKey: 'competition_id', as: 'competition' });
 CompetitionRegistration.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
 User.hasMany(CompetitionRegistration, { foreignKey: 'user_id', as: 'competitionRegistrations' });
-
-// Round players / scores
 RoundPlayer.belongsTo(Round, { foreignKey: 'round_id', as: 'round' });
 RoundPlayer.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
 RoundPlayer.hasMany(HoleScore, { foreignKey: 'round_player_id', as: 'holeScores' });
-
 HoleScore.belongsTo(RoundPlayer, { foreignKey: 'round_player_id', as: 'roundPlayer' });
 HoleScore.belongsTo(User, { foreignKey: 'updated_by', as: 'updater' });
-
 IndexHistory.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
 IndexHistory.belongsTo(Round, { foreignKey: 'round_id', as: 'round' });
-
 User.hasMany(Article, { foreignKey: 'created_by', as: 'articles' });
 Article.belongsTo(User, { foreignKey: 'created_by', as: 'author' });
-
-// Match Play
 MatchPlayChampionship.belongsTo(User, { foreignKey: 'created_by', as: 'creator', constraints: false });
 MatchPlayChampionship.hasMany(MatchPlayMatch, { foreignKey: 'championship_id', as: 'matches', constraints: false });
 MatchPlayMatch.belongsTo(MatchPlayChampionship, { foreignKey: 'championship_id', as: 'championship', constraints: false });
-// constraints: false → permet player_a/b NULL (byes / en attente) sans FK MySQL bloquante
 MatchPlayMatch.belongsTo(User, { foreignKey: 'player_a_id', as: 'playerA', constraints: false });
 MatchPlayMatch.belongsTo(User, { foreignKey: 'player_b_id', as: 'playerB', constraints: false });
 MatchPlayMatch.belongsTo(User, { foreignKey: 'winner_id', as: 'winner', constraints: false });
 MatchPlayMatch.belongsTo(User, { foreignKey: 'created_by', as: 'creator', constraints: false });
 AccountingEntry.belongsTo(User, { foreignKey: 'created_by', as: 'creator', constraints: false });
 Invoice.belongsTo(User, { foreignKey: 'created_by', as: 'creator', constraints: false });
-
-
 Round.hasMany(RoundComment, { foreignKey: 'round_id', as: 'comments', constraints: false });
 RoundComment.belongsTo(Round, { foreignKey: 'round_id', as: 'round', constraints: false });
 RoundComment.belongsTo(User, { foreignKey: 'user_id', as: 'user', constraints: false });
@@ -98,11 +82,7 @@ Round.hasMany(RoundExploit, { foreignKey: 'round_id', as: 'exploits', constraint
 RoundExploit.belongsTo(Round, { foreignKey: 'round_id', as: 'round', constraints: false });
 RoundExploit.belongsTo(User, { foreignKey: 'user_id', as: 'user', constraints: false });
 RoundExploit.belongsTo(User, { foreignKey: 'created_by', as: 'creator', constraints: false });
-
-
-
 Club.hasMany(User, { foreignKey: 'club_id', as: 'players', constraints: false });
-
 if (MarcassinsEdition && MarcassinsTeam) {
   MarcassinsEdition.belongsTo(Course, { foreignKey: 'course_id', as: 'course', constraints: false });
   MarcassinsEdition.belongsTo(User, { foreignKey: 'created_by', as: 'creator', constraints: false });
@@ -117,16 +97,20 @@ if (MarcassinsEdition && MarcassinsRegistration) {
   MarcassinsRegistration.belongsTo(MarcassinsEdition, { foreignKey: 'edition_id', as: 'edition' });
   MarcassinsRegistration.belongsTo(User, { foreignKey: 'user_id', as: 'user', constraints: false });
 }
-
 Article.hasMany(ArticleLike, { foreignKey: 'article_id', as: 'likes', constraints: false });
 Article.hasMany(ArticleComment, { foreignKey: 'article_id', as: 'comments', constraints: false });
 ArticleLike.belongsTo(Article, { foreignKey: 'article_id', as: 'article', constraints: false });
 ArticleLike.belongsTo(User, { foreignKey: 'user_id', as: 'user', constraints: false });
 ArticleComment.belongsTo(Article, { foreignKey: 'article_id', as: 'article', constraints: false });
 ArticleComment.belongsTo(User, { foreignKey: 'user_id', as: 'user', constraints: false });
-
 ChatMessage.belongsTo(User, { foreignKey: 'user_id', as: 'user', constraints: false });
 User.belongsTo(Club, { foreignKey: 'club_id', as: 'club', constraints: false });
+BestiolesSeason.hasMany(BestiolesFixture, { foreignKey: 'season_id', as: 'fixtures', constraints: false });
+BestiolesFixture.belongsTo(BestiolesSeason, { foreignKey: 'season_id', as: 'season', constraints: false });
+BestiolesFixture.belongsTo(Club, { foreignKey: 'home_club_id', as: 'homeClub', constraints: false });
+BestiolesFixture.belongsTo(Club, { foreignKey: 'away_club_id', as: 'awayClub', constraints: false });
+User.hasMany(PushSubscription, { foreignKey: 'user_id', as: 'pushSubs', constraints: false });
+PushSubscription.belongsTo(User, { foreignKey: 'user_id', as: 'user', constraints: false });
 
 module.exports = {
   sequelize,
@@ -153,12 +137,8 @@ module.exports = {
   Invoice,
   RoundComment,
   RoundExploit,
+  PushSubscription,
   MarcassinsEdition,
   MarcassinsTeam,
   MarcassinsRegistration,
 };
-
-BestiolesSeason.hasMany(BestiolesFixture, { foreignKey: 'season_id', as: 'fixtures', constraints: false });
-BestiolesFixture.belongsTo(BestiolesSeason, { foreignKey: 'season_id', as: 'season', constraints: false });
-BestiolesFixture.belongsTo(Club, { foreignKey: 'home_club_id', as: 'homeClub', constraints: false });
-BestiolesFixture.belongsTo(Club, { foreignKey: 'away_club_id', as: 'awayClub', constraints: false });
